1a55ba878c56dca7307e62644117fc7d9911ac62
maxime-palleja <maxime.palleja@gmail.com>
2023-03-31 11:48:24 +0200

    11h48

--- src/app/liste-chat/liste-chat.component.ts
+++ src/app/liste-chat/liste-chat.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {AngularFirestore} from "@angular/fire/compat/firestore";
+import {AngularFireAuth} from "@angular/fire/compat/auth";
 
 @Component({
   selector: 'app-liste-chat',
@@ -6,10 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./liste-chat.component.scss']
 })
 export class ListeChatComponent implements OnInit {
-
-  constructor() { }
+  userId!: string ;
+  conversations: any[] = [];
+  constructor(private afs: AngularFirestore,private auth: AngularFireAuth) { }
 
   ngOnInit(): void {
+    this.auth.authState.subscribe(user => {if (user) {this.userId= user.uid;}});
+    this.afs.collection('CONVERSATIONS', ref => ref.where('idClient', '==', this.userId)).valueChanges().subscribe(conversations => {
+      this.conversations = conversations;
+      
+    });
   }
 
 }
