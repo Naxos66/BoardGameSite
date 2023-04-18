bbb7a37a07020a86c193edc8672fd8ace9f193f5
maxime-palleja <maxime.palleja@gmail.com>
2023-04-18 15:18:02 +0200

    last

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -82,7 +82,7 @@ const routes: Routes = [
     canActivate: [AuthGuard]
   },
   {
-    path: 'discussion/:idLoueur&:idJeu&:idLoueur',
+    path: 'discussion/:idLoueur/:idJeu:/idLoueur',
     component: DiscussionComponent,
     canActivate: [AuthGuard]
   },
